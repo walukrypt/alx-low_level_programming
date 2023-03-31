cc866cd67b2a838702632b7b106b75be7fd24d4b
walukrypt <krptickinz@gmail.com>
2023-03-31 10:12:05 +0100

    Update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,27 @@
- /**
-  * _strcat-concatenates two strings
-  * @dest: input value
-  * @src: input value
-  * Return: void
-  */
+#include "main.h"
 
- char *_strcat(char *dest, char *src
-     {
-      int i;
-      int j;
+/**
+ * *_strcat - Entry point
+ * Description: Concatenates two strings
+ * @dest: Character
+ * @src: Character
+ * Return: char
+ */
 
-      i=0;
-      while(dest[i] != '\0')
-        {
-          i++;
-        }
-      j=0;
-      while(src[j] != '\0')
-        {
-           dest[i] = src[j];
-           i++;
-           j++;
-        }
-      dest[i] = '\0';
-      return(dest);
-     }      
+char *_strcat(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
+	return (dest);
+}
