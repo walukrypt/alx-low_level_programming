d1a43bcce8def86db1379d9c96d88371fbd5ad46
walukrypt <kryptickinz@gmail.com>
2023-04-05 10:25:03 -0700

    0-memset.c

--- 0-memset.c
+++ 0-memset.c
@@ -1,46 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
+ * *_memset - Entry point
+ * Description: Fills memory with a constant byte
+ * @s: Character
+ * @b: Character
+ * @n: Integer
+ * Return: char
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+
+char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	char *p = s;
 
-        i = 0;
-        while (i < size)
-        {
-                if (i % 10)
-                {
-                        printf(" ");
-                }
-                if (!(i % 10) && i)
-                {
-                        printf("\n");
-                }
-                printf("0x%02x", buffer[i]);
-                i++;
-        }
-        printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char buffer[98] = {0x00};
-
-    simple_print_buffer(buffer, 98);
-    _memset(buffer, 0x01, 95);
-    printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);    
-    return (0);
+	while (n-- > 0)
+	{
+		*p++ = b;
+	}
+	return (s);
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x06. C - More pointers, arrays and strings
+0x07. C - Even more pointers, arrays and strings
